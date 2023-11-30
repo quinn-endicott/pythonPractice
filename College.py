a7c5b782137af1f433710ddfef35f01b6302e992
Quinnnnnnnnnnnnnnnnnnn <quinn.endicott@gmail.com>
2023-11-30 13:57:53 -0500

    Working on CRUD program for student table
    insert statement works

--- College.py
+++ College.py
@@ -22,3 +22,32 @@ def createCollege():
     finally:
         if conn != None:
             conn.close()
+
+def stuCRUD():
+    pass
+def depCRUD():
+    pass
+def majCRUD():
+    pass
+
+def stuOptions():
+    print('Options:')
+    print('Add a Student')
+    print('Search for a Student')
+    print('Change Student information')
+    print('Delete Student Records')
+    print('Show all Students')
+def depOptions():
+    print('Options:')
+    print('Add a Department')
+    print('Search for a Department')
+    print('Change Department information')
+    print('Delete Department Records')
+    print('Show all Departments')
+def majOptions():
+    print('Options:')
+    print('Add a Major')
+    print('Search for a Major')
+    print('Change Major information')
+    print('Delete Major Records')
+    print('Show all Majors')
